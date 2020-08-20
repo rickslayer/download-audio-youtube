9068a7f68c07d37d28a31fe875b2a54c7750c392
rickslayer <paulo.rube@madeiramadeira.com.br>
2020-08-20 09:51:33 -0300

    APIs corrections

--- src/api/app.py
+++ src/api/app.py
@@ -1,17 +1,22 @@
+from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import boto3
 from botocore.exceptions import ClientError
 from botocore.client import Config
 from pytube import YouTube
-from moviepy.editor import *
 import os
 
 app = Flask(__name__)
 CORS(app)
+load_dotenv()
+
 
 @app.route('/', methods=['POST'])
-def download_yb():
+def download_yb() -> dict():
+    '''
+        Responsible for download video
+    '''
     param = request.get_json()
     url = param.get('url')
     yt = YouTube(url)
@@ -22,28 +27,35 @@ def download_yb():
     path = "./download/"
     os.rename(path+name+".mp4", path+new_name+".mp4")
     obj_name = new_name+".mp4"
-    obj_name_mp3 = new_name+".mp3"
 
-    video = VideoFileClip(path+obj_name)
-    audio = video.audio
-    audio.write_audiofile(path+obj_name_mp3)
-    audio.close()
-    video.close()
+    try:
 
-    s3 = boto3.client('s3',aws_access_key_id='AKIAWLHG4IBYCERUB7PC', aws_secret_access_key='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', config=Config(signature_version='s3v4'))
-    s3.upload_file('./download/'+obj_name_mp3,'prra-youtube-audio', obj_name_mp3)
+        s3 = boto3.client(
+            's3',
+            aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+            config=Config(signature_version='s3v4'))
 
-    os.remove(path+obj_name_mp3)
+        s3.upload_file(
+            './download/'+obj_name,
+            os.environ.get('AWS_BUCKET'),
+            obj_name)
 
-    response = s3.generate_presigned_url('get_object',
-                                                Params={'Bucket': 'prra-youtube-audio',
-                                                        'Key': obj_name_mp3},
-                                                ExpiresIn=3600)
+        os.remove(path+obj_name)
 
-    return jsonify(
-        url=url,
-        name=name,
-        new_name=new_name,
-        download_url=response
-    )
+        download_url = s3.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': os.environ.get('AWS_BUCKET'), 'Key': obj_name},
+            ExpiresIn=3600)
 
+        return jsonify(
+            url=url,
+            name=name,
+            new_name=new_name,
+            download_url=download_url
+        )
+    except ClientError as e:
+        return jsonify(
+            error=True,
+            message=e
+        )
